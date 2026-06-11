6a2be887aa2f816e0afa7f5f5f20b128f9fd6e44
Gileesa <gileesa@icloud.com>
2026-06-11 14:49:38 +0200

    small tetsstt

--- test.py
+++ test.py
@@ -2,4 +2,13 @@
 # testing github repo and if snellius will run this
 #
 
+#
+#
+
+# Run
+
+#   git config --global user.email "gileesa@icloud.com"
+#   git config --global user.name "Gileesa"
+
+
 print('hello snellius')
