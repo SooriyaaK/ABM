cb5398dd2e2e1f6eb33000c69b17454ce1a03f6b
Gileesa <gileesa@icloud.com>
2026-06-17 13:19:11 +0200

    added comments and doc strings

--- Convergence_discrete.py
+++ Convergence_discrete.py
@@ -13,7 +13,15 @@ from typing import List
 
 
 def f_overall_entropy(agents) -> float:
-    """Compute global entropy E_T over income groups, all agents."""
+    """
+    Compute global entropy E_T over income groups, all agents
+    This value will be used to compute the Multigroup Entropy Index
+    
+    Params:
+    - agents: all agents in the simulation
+    Returns:
+    - Et: float of the global entropy over income groups, over all neighbourhoods
+    """
     incomes = np.array([a.income for a in agents])
     if len(incomes) == 0: # no agents -> return zero
         return 0.0
@@ -22,7 +30,7 @@ def f_overall_entropy(agents) -> float:
     values, counts = np.unique(incomes, return_counts=True)
     p_g = counts / counts.sum()  # fraction for each group
 
-    # avoid log2(0): mask sum for p_g > 0
+    # avoid log2(0): only keep terms with p_g > 0
     mask = p_g > 0
     p_g = p_g[mask]
 
@@ -31,7 +39,15 @@ def f_overall_entropy(agents) -> float:
 
 
 def f_neighbourhood_entropy(neighbourhood) -> float:
-    """Compute neighbourhood entropy E_n over income groups, only in this neighbourhood."""
+    """
+    Compute neighbourhood entropy E_n over income groups, only in this neighbourhood.
+    This value will be used to compute the Multigroup Entropy Index
+
+    Params:
+    - neighbourhood: one Neighbourhood object
+    Returns:
+    - En: float of the entropy over income groups, only in this neighbourhood
+    """
     agents = neighbourhood.agents
     incomes = np.array([a.income for a in agents])
     if len(incomes) == 0: # no agents -> return zero
@@ -40,6 +56,7 @@ def f_neighbourhood_entropy(neighbourhood) -> float:
     values, counts = np.unique(incomes, return_counts=True)
     p_ng = counts / counts.sum() # fraction for each group
 
+    # avoid log2(0): only keep terms with p_g > 0
     mask = p_ng > 0 # mask zeroes
     p_ng = p_ng[mask]
 
@@ -48,8 +65,20 @@ def f_neighbourhood_entropy(neighbourhood) -> float:
 
 
 def compute_H(neighbourhoods, agents) -> float:
+    """
+    Computes Multigroup Entropy Index H; i.e segregation metric for macro-neighbourhoods.
+    Works for discrete income groups, can be either numerical (1,2,3) or non-numeric (e.g strings).
+
+    H is in [0, 1], with 0 = no segregation & values closer to 1 = higher segregation.
+
+    Params:
+    - neighbourhoods: list of Neighbourhood objects with all neighbourhoods in the simulation
+    - agents: list of all agents in the simulation
+    Returns:
+    - H: float of the Multigroup Entropy Index for one iteration of the simulation
+    """
     Et = f_overall_entropy(agents)
-    if Et == 0.0: # no agents
+    if Et == 0.0: # no agents OR only one income group globally
         return 0.0
 
     T = len(agents) # total number of agents
