7b061a28246dc5ee5490c591adf10b14e354e5e7
uva-hva.gitlab.host:alara.karadeniz@student.uva.nl <alara.karadeniz@student.uva.nl>
2026-06-21 17:58:43 +0200

    changed the params file to have the actually params we want

--- Params.py
+++ Params.py
@@ -6,15 +6,17 @@ import json
 import numpy as np
 
 # the values we want to vary with (min, max, number)
-homophily_values     = np.linspace(0, 1, 10).tolist()
+density_values     = np.linspace(0, 1, 10).tolist()
 defector_frac_values = np.linspace(0, 1, 10).tolist()
-learning_rate_values = np.linspace(0, 1, 10).tolist()
+neighbourhood_count_values = np.linspace(5, 5, 250).tolist()
+activation_rate_values = np.linspace(0, 1, 10).tolist()
 
 combos = [
-    {"homophily": round(h, 4), "defector_frac": round(d, 4), "learning_rate": round(l, 4)}
-    for h in homophily_values
+    {"density": round(h, 4), "defector_frac": round(d, 4), "neighbourhood_count": round(l, 4), "activation_rate": round(s, 4)}
+    for h in density_values
     for d in defector_frac_values
-    for l in learning_rate_values
+    for l in neighbourhood_count_values
+    for s in activation_rate_values
 ]
 
 with open("params.json", "w") as f:
